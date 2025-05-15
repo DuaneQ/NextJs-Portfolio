539db40b9791425421bd4f93e462fa8ad43d3836
The boss <duaneqhodges@gmail.com>
2025-05-15 17:50:48 -0400

    Fix text overflow issue on About page

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
-  basePath: "/NextJs-Portfolio", // Use your repository name here
+  basePath: isProd ? "/NextJs-Portfolio" : "", // Use basePath only in production
   env: {
-    NEXT_PUBLIC_BASE_PATH: "/NextJs-Portfolio",
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/NextJs-Portfolio" : "", // Expose basePath for client-side use
   },
 };
 
--- src/app/components/about/index.jsx
+++ src/app/components/about/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import clsx from "clsx";
 
 const ItemLayout = ({ children, className }) => {
@@ -6,9 +6,8 @@ const ItemLayout = ({ children, className }) => {
         <div className={clsx('custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center', className)}>
             {children}
         </div>
-    )
-}
-
+    );
+};
 
 const AboutDetails = () => {
     return (
@@ -16,10 +15,10 @@ const AboutDetails = () => {
             <div className='grid grid-cols-12 gap-8 xs:gap-6 md:gap-8 w-full'>
 
                 <ItemLayout className={'col-span-full lg:col-span-8 row-span-2 flex-col items-start'}>
-                    <h2 className='text-xl md:text-4xl text-left w-full capitalize'>
+                    <h2 className='text-lg md:text-1xl text-left w-full capitalize'>
                         May the source code be with you
                     </h2>
-                    <p className='font-light sm:text-sm md:text-base text-xs'>
+                    <p className='font-light text-xs sm:text-sm md:text-base'>
                         Proven expertise in building scalable web applications, creating deployment pipelines with quality gates,
                         and leveraging modern technologies like React, Firebase, and Next.js to deliver innovative solutions.
                         Skilled in cloud platforms such as AWS, Azure, and GCP, with a strong focus on data integrity,
@@ -27,16 +26,9 @@ const AboutDetails = () => {
                     </p>
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-4 text-accent'}>
-                    <div className='font-semibold w-full text-left text-1xl sm:text-5xl'>
-                        Multiple Industries <sub className='font-semibold text-base'>industries</sub>
-                    </div>
-                </ItemLayout>
-
-                <ItemLayout className={'col-span-4 text-accent'}>
-
-                    <div className='font-semibold w-full text-left text-1xl sm:text-5xl'>
-                        15+ <sub className='font-semibold text-base'>years of experience</sub>
+                <ItemLayout className={'col-span-full md:col-span-6 !p-0'}>
+                    <div className='font-semibold text-sm'>
+                        18+ years of experience
                     </div>
                 </ItemLayout>
 
@@ -71,7 +63,7 @@ const AboutDetails = () => {
                 </ItemLayout>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default AboutDetails
+export default AboutDetails;
